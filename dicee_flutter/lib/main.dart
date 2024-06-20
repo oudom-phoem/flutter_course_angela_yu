979e7b3240074743a30392bea245cf29b9d464fb
Oudom Phoem <oudomphoem@gmail.com>
2024-06-21 00:51:10 +0700

    use variable for images number

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -15,17 +15,18 @@ void main() {
           ),
           backgroundColor: Colors.red,
         ),
-        body: const DicePage(),
+        body: DicePage(),
       ),
     ),
   );
 }
 
 class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+  DicePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var leftDiceNumber = 5;
     return Center(
       child: Row(
         children: [
@@ -34,7 +35,7 @@ class DicePage extends StatelessWidget {
               onPressed: () {
                 print('Left button got pressed.');
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
