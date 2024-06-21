f11bb1a956eb11ce61f1565f447e45d67605d687
Oudom Phoem <oudomphoem@gmail.com>
2024-06-21 19:25:36 +0700

    Make the image interactive when got click

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -38,14 +39,18 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: TextButton(
         onPressed: () {
-          print('I got clicked');
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
