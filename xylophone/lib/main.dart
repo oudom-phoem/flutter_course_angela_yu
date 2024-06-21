7503320f64ec5371e777314c23c86f6a57fedfa6
Oudom Phoem <oudomphoem@gmail.com>
2024-06-21 23:54:00 +0700

    Create 7 buttons playing sounds

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -8,20 +8,90 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  playSound(1);
+                },
+                child: Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.red,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.orange,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.yellow,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.green,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.teal,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.blue,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Container(
+                  width: 100,
+                  height: 50,
+                  color: Colors.purple,
+                ),
+              ),
+            ],
           ),
         ),
       ),
