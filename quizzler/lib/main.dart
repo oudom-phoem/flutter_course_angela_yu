291791d955f2a5f5c46cd73b53676ec0650cac90
Oudom Phoem <oudomphoem@gmail.com>
2024-06-22 21:18:13 +0700

    Initial

---
+++ quizzler/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Quizzer());
+}
+
+class Quizzer extends StatelessWidget {
+  const Quizzer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: QuizPage(),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
