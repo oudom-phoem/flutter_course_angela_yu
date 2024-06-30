ddab9f92375d821cdfcc5af7ce3762f17606f2a6
Oudom Phoem <oudomphoem@gmail.com>
2024-06-30 14:53:59 +0700

    Step 4 Complete

--- destini/lib/story.dart
+++ destini/lib/story.dart
@@ -4,6 +4,7 @@ class Story {
   String storyTitle;
   String choice1;
   String choice2;
+  //TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
 }
-
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
